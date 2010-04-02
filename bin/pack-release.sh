fc05ec7509fd928a7e3e607ef7e48ca95210c6b5
Kohei Yoshida <kyoshida@novell.com>
2010-04-02 16:58:39 -0400

    Don't include .hgtags in the release package.

--- bin/pack-release.sh
+++ bin/pack-release.sh
@@ -4,6 +4,7 @@ VERSION=0.1.2
 DIR=mdds_$VERSION
 hg clone https://multidimalgorithm.googlecode.com/hg/ $DIR
 rm -rf $DIR/.hg
+rm -f $DIR/.hgtags
 rm -rf $DIR/bin
 find $DIR -name '*.vp?' -type f | xargs rm -f
 tar jcvf $DIR.tar.bz2 $DIR
