c61b82409c9219103c35bd372c77cca875983cc2
Kohei Yoshida <kyoshida@novell.com>
2010-04-09 21:16:17 -0400

    Added simple example file on how to use segment_tree data structure.

---
+++ example/segment_tree.cpp
@@ -0,0 +1,39 @@
+
+#include <mdds/segmenttree.hpp>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+typedef ::mdds::segment_tree<long, string> db_type;
+
+struct string_printer : public unary_function<string*, void>
+{
+    void operator() (const string* p) const
+    {
+        cout << "search hit: " << *p << endl;
+    }
+};
+
+int main()
+{
+    db_type db;
+    string A("A");
+    string B("B");
+    string C("C");
+
+    // Insert data into the tree.
+    db.insert(0,  10, &A);
+    db.insert(2,  20, &B);
+    db.insert(10, 15, &C);
+
+    // Don't forget to build it before calling search().
+    db.build_tree();
+
+    // Run search and get the result.
+    db_type::search_result result = db.search(5);
+
+    // Print the result.
+    cout << "result size: " << result.size() << endl;
+    for_each(result.begin(), result.end(), string_printer());
+}
