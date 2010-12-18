35979c21e2844042c0b6c2d22d2d27d1bb463ac5
Kohei Yoshida <kyoshida@novell.com>
2010-12-18 00:09:59 -0500

    Moved the storage class implementation up.

--- include/mdds/mixed_type_matrix.hpp
+++ include/mdds/mixed_type_matrix.hpp
@@ -74,136 +74,6 @@ public:
 
     typedef ::mdds::element<string_type> element;
 
-    /**
-     * Default constructor.
-     */
-    mixed_type_matrix();
-
-    /**
-     * Construct an empty matrix with specified density type.
-     */
-    mixed_type_matrix(matrix_density_t density);
-
-    /**
-     * Construct a matrix of specified size with specified density type. 
-     */
-    mixed_type_matrix(size_t rows, size_t cols, matrix_density_t density);
-
-    mixed_type_matrix(const mixed_type_matrix& r);
-    ~mixed_type_matrix();
-
-    mixed_type_matrix& operator= (const mixed_type_matrix& r);
-
-    /**
-     * Get the type of element specified by its position.  The type can be one 
-     * of empty, string, numeric, or boolean. 
-     * 
-     * @return element type.
-     */
-    matrix_element_t get_type(size_t row, size_t col) const;
-
-    double get_numeric(size_t row, size_t col) const;
-    bool get_boolean(size_t row, size_t col) const;
-    const string_type* get_string(size_t row, size_t col) const;
-
-    void set_numeric(size_t row, size_t col, double val);
-    void set_boolean(size_t row, size_t col, bool val);
-    void set_string(size_t row, size_t col, string_type* str);
-    void set_empty(size_t row, size_t col);
-
-    void set(size_t row, size_t col, double val);
-    void set(size_t row, size_t col, bool val);
-    void set(size_t row, size_t col, string_type* str);
-
-    /**
-     * Set flag value at specified position.
-     * 
-     * @param row row position
-     * @param col column position
-     * @param flag_type flag value
-     */
-    void set_flag(size_t row, size_t col, flag_type flag);
-
-    /**
-     * Get flag value at specified position. 
-     *  
-     * @param row row position 
-     * @param col column position 
-     * 
-     * @return flag value stored at specified position
-     */
-    flag_type get_flag(size_t row, size_t col) const;
-
-    void clear_flag(size_t row, size_t cols);
-
-    /**
-     * Return the size of matrix as a pair.  The first value is the row size,
-     * while the second value is the column size.
-     * 
-     * @return matrix size as a value pair.
-     */
-    size_pair_type size() const;
-    
-    /** 
-     * Transpose the stored matrix data. 
-     *  
-     * @return reference to this matrix instance. 
-     */
-    mixed_type_matrix& transpose();
-
-    /**
-     * Assign values from the passed matrix instance.  If the size of the
-     * passed matrix is smaller, then the element values are assigned by their
-     * positions, while the rest of the elements that fall outside the size of
-     * the passed matrix instance will remain unmodified.  If the size of the
-     * pass matrix instance is larger, then only the elements within the size
-     * of this matrix instance will get assigned.
-     * 
-     * @param r passed matrix object to assign element values from.
-     */
-    void assign(const mixed_type_matrix& r);
-
-    /**
-     * Resize the matrix to specified size.  This method supports resizing to
-     * zero-sized matrix; however, either specifying the row or column size to
-     * zero will resize the matrix to 0 x 0.
-     * 
-     * @param row new row size
-     * @param col new column size
-     */
-    void resize(size_t row, size_t col);
-
-    /**
-     * Empty the matrix.
-     */
-    void clear();
-
-    /**
-     * Check whether or not this matrix is numeric.  A numeric matrix contains 
-     * only numeric or boolean elements. 
-     * 
-     * @return true if the matrix contains only numeric or boolean elements, 
-     *         or false otherwise.
-     */
-    bool numeric() const;
-
-    /**
-     * Check whether or not this matrix is empty. 
-     * 
-     * @return true if this matrix is empty, or false otherwise.
-     */
-    bool empty() const;
-
-    /**
-     * Swap the content of the matrix with another instance.
-     */
-    void swap(mixed_type_matrix& r);
-
-#ifdef UNIT_TEST
-    void dump() const;
-    void dump_flags() const;
-#endif
-
 private:
     struct size_pair_type_hash
     {
@@ -821,8 +691,139 @@ private:
         bool        m_valid:1;
     };
 
-private:
     static storage_base* create_storage(size_t rows, size_t cols, matrix_density_t density);
+
+public:
+
+    /**
+     * Default constructor.
+     */
+    mixed_type_matrix();
+
+    /**
+     * Construct an empty matrix with specified density type.
+     */
+    mixed_type_matrix(matrix_density_t density);
+
+    /**
+     * Construct a matrix of specified size with specified density type. 
+     */
+    mixed_type_matrix(size_t rows, size_t cols, matrix_density_t density);
+
+    mixed_type_matrix(const mixed_type_matrix& r);
+    ~mixed_type_matrix();
+
+    mixed_type_matrix& operator= (const mixed_type_matrix& r);
+
+    /**
+     * Get the type of element specified by its position.  The type can be one 
+     * of empty, string, numeric, or boolean. 
+     * 
+     * @return element type.
+     */
+    matrix_element_t get_type(size_t row, size_t col) const;
+
+    double get_numeric(size_t row, size_t col) const;
+    bool get_boolean(size_t row, size_t col) const;
+    const string_type* get_string(size_t row, size_t col) const;
+
+    void set_numeric(size_t row, size_t col, double val);
+    void set_boolean(size_t row, size_t col, bool val);
+    void set_string(size_t row, size_t col, string_type* str);
+    void set_empty(size_t row, size_t col);
+
+    void set(size_t row, size_t col, double val);
+    void set(size_t row, size_t col, bool val);
+    void set(size_t row, size_t col, string_type* str);
+
+    /**
+     * Set flag value at specified position.
+     * 
+     * @param row row position
+     * @param col column position
+     * @param flag_type flag value
+     */
+    void set_flag(size_t row, size_t col, flag_type flag);
+
+    /**
+     * Get flag value at specified position. 
+     *  
+     * @param row row position 
+     * @param col column position 
+     * 
+     * @return flag value stored at specified position
+     */
+    flag_type get_flag(size_t row, size_t col) const;
+
+    void clear_flag(size_t row, size_t cols);
+
+    /**
+     * Return the size of matrix as a pair.  The first value is the row size,
+     * while the second value is the column size.
+     * 
+     * @return matrix size as a value pair.
+     */
+    size_pair_type size() const;
+    
+    /** 
+     * Transpose the stored matrix data. 
+     *  
+     * @return reference to this matrix instance. 
+     */
+    mixed_type_matrix& transpose();
+
+    /**
+     * Assign values from the passed matrix instance.  If the size of the
+     * passed matrix is smaller, then the element values are assigned by their
+     * positions, while the rest of the elements that fall outside the size of
+     * the passed matrix instance will remain unmodified.  If the size of the
+     * pass matrix instance is larger, then only the elements within the size
+     * of this matrix instance will get assigned.
+     * 
+     * @param r passed matrix object to assign element values from.
+     */
+    void assign(const mixed_type_matrix& r);
+
+    /**
+     * Resize the matrix to specified size.  This method supports resizing to
+     * zero-sized matrix; however, either specifying the row or column size to
+     * zero will resize the matrix to 0 x 0.
+     * 
+     * @param row new row size
+     * @param col new column size
+     */
+    void resize(size_t row, size_t col);
+
+    /**
+     * Empty the matrix.
+     */
+    void clear();
+
+    /**
+     * Check whether or not this matrix is numeric.  A numeric matrix contains 
+     * only numeric or boolean elements. 
+     * 
+     * @return true if the matrix contains only numeric or boolean elements, 
+     *         or false otherwise.
+     */
+    bool numeric() const;
+
+    /**
+     * Check whether or not this matrix is empty. 
+     * 
+     * @return true if this matrix is empty, or false otherwise.
+     */
+    bool empty() const;
+
+    /**
+     * Swap the content of the matrix with another instance.
+     */
+    void swap(mixed_type_matrix& r);
+
+#ifdef UNIT_TEST
+    void dump() const;
+    void dump_flags() const;
+#endif
 
 private:
     storage_base* mp_storage;
