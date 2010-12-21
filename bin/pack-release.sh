415fbfb3a8558545e6816d7aa83a420b02570059
Kohei Yoshida <kyoshida@novell.com>
2010-12-20 22:20:10 -0500

    Get version info from VERSION file, not from configure.ac.
    This way I can remove the bin directory from the source package.

--- bin/pack-release.sh
+++ bin/pack-release.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-VERSION=@VERSION@
+PROGDIR=`dirname $0`
+VERSION=`cat $PROGDIR/../VERSION`
 DIR=mdds_$VERSION
 hg clone https://multidimalgorithm.googlecode.com/hg/ $DIR
 pushd .
 cd $DIR
 autoconf
-rm -rf autom4te.cache .hg .hgtags autogen.sh configure.ac
+rm -rf autom4te.cache .hg .hgtags bin autogen.sh configure.ac
 popd
 find $DIR -name '*.vp?' -type f | xargs rm -f
 tar jcvf $DIR.tar.bz2 $DIR
