56ce3b305456cfbbc6306bc7a035e1e21905af71
Kohei Yoshida <kyoshida@novell.com>
2011-07-22 18:12:08 -0400

    Adjusted package script for git.

--- bin/pack-release.sh
+++ bin/pack-release.sh
@@ -3,11 +3,11 @@
 PROGDIR=`dirname $0`
 VERSION=`cat $PROGDIR/../VERSION`
 DIR=mdds_$VERSION
-hg clone https://multidimalgorithm.googlecode.com/hg/ $DIR
+git clone https://code.google.com/p/multidimalgorithm/ $DIR
 pushd .
 cd $DIR
 autoconf
-rm -rf autom4te.cache .hg .hgtags bin
+rm -rf autom4te.cache .git bin
 popd
 find $DIR -name '*.vp?' -type f | xargs rm -f
 tar jcvf $DIR.tar.bz2 $DIR
