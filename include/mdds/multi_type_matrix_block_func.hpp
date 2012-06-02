a404d90e2e18edeace054d1fcc6046a95523e1b0
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-06-02 14:02:03 -0400

    These methods must be static.

--- include/mdds/multi_type_matrix_block_func.hpp
+++ include/mdds/multi_type_matrix_block_func.hpp
@@ -40,63 +40,63 @@ struct trait
     typedef _StringType string_type;
     typedef mdds::mtv::default_element_block<element_type_mtx_string, string_type> string_cell_block;
 
-    mdds::mtv::element_t mdds_mtv_get_element_type(const string_type&)
+    static mdds::mtv::element_t mdds_mtv_get_element_type(const string_type&)
     {
         return element_type_mtx_string;
     }
 
-    void mdds_mtv_set_value(mtv::base_element_block& block, size_t pos, const string_type& val)
+    static void mdds_mtv_set_value(mtv::base_element_block& block, size_t pos, const string_type& val)
     {
         string_cell_block::set_value(block, pos, val);
     }
 
-    void mdds_mtv_get_value(const mtv::base_element_block& block, size_t pos, string_type& val)
+    static void mdds_mtv_get_value(const mtv::base_element_block& block, size_t pos, string_type& val)
     {
         string_cell_block::get_value(block, pos, val);
     }
 
     template<typename _Iter>
-    void mdds_mtv_set_values(
+    static void mdds_mtv_set_values(
         mtv::base_element_block& block, size_t pos, const string_type&, const _Iter& it_begin, const _Iter& it_end)
     {
         string_cell_block::set_values(block, pos, it_begin, it_end);
     }
 
-    void mdds_mtv_append_value(mtv::base_element_block& block, const string_type& val)
+    static void mdds_mtv_append_value(mtv::base_element_block& block, const string_type& val)
     {
         string_cell_block::append_value(block, val);
     }
 
-    void mdds_mtv_prepend_value(mtv::base_element_block& block, const string_type& val)
+    static void mdds_mtv_prepend_value(mtv::base_element_block& block, const string_type& val)
     {
         string_cell_block::prepend_value(block, val);
     }
 
     template<typename _Iter>
-    void mdds_mtv_prepend_values(mtv::base_element_block& block, const string_type&, const _Iter& it_begin, const _Iter& it_end)
+    static void mdds_mtv_prepend_values(mtv::base_element_block& block, const string_type&, const _Iter& it_begin, const _Iter& it_end)
     {
         string_cell_block::prepend_values(block, it_begin, it_end);
     }
 
     template<typename _Iter>
-    void mdds_mtv_append_values(mtv::base_element_block& block, const string_type&, const _Iter& it_begin, const _Iter& it_end)
+    static void mdds_mtv_append_values(mtv::base_element_block& block, const string_type&, const _Iter& it_begin, const _Iter& it_end)
     {
         string_cell_block::append_values(block, it_begin, it_end);
     }
 
     template<typename _Iter>
-    void mdds_mtv_assign_values(mtv::base_element_block& dest, const string_type&, const _Iter& it_begin, const _Iter& it_end)
+    static void mdds_mtv_assign_values(mtv::base_element_block& dest, const string_type&, const _Iter& it_begin, const _Iter& it_end)
     {
         string_cell_block::assign_values(dest, it_begin, it_end);
     }
 
-    void mdds_mtv_get_empty_value(string_type& val)
+    static void mdds_mtv_get_empty_value(string_type& val)
     {
         val = string_type();
     }
 
     template<typename _Iter>
-    void mdds_mtv_insert_values(
+    static void mdds_mtv_insert_values(
         mtv::base_element_block& block, size_t pos, string_type, const _Iter& it_begin, const _Iter& it_end)
     {
         string_cell_block::insert_values(block, pos, it_begin, it_end);
