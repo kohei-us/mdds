cc42aa8ff4f3871ec2086386a1f41fcbecc8d8bc
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-12-26 16:15:51 -0500

    multi_type_vector: common base class for const and non-const iterators.
    To remove duplicated code from these two iterator classes.

--- include/mdds/multi_type_vector_itr.hpp
+++ include/mdds/multi_type_vector_itr.hpp
@@ -28,8 +28,9 @@
 namespace mdds { namespace __mtv {
 
 template<typename _ParentType, typename _BlksType, typename _BaseItrType>
-class iterator_base
+class iterator_common_base
 {
+protected:
     typedef _ParentType parent_type;
     typedef _BlksType blocks_type;
     typedef _BaseItrType base_iterator_type;
@@ -44,10 +45,55 @@ class iterator_base
         node(const node& other) : type(other.type), size(other.size), data(other.data) {}
     };
 
+    iterator_common_base() {}
+
+    iterator_common_base(const base_iterator_type& pos, const base_iterator_type& end) :
+        m_pos(pos), m_end(end)
+    {
+        if (m_pos != m_end)
+            update_node();
+    }
+
+    iterator_common_base(const iterator_common_base& other) :
+        m_pos(other.m_pos), m_end(other.m_end)
+    {
+        if (m_pos != m_end)
+            update_node();
+    }
+
+    void update_node()
+    {
+        // blocks_type::value_type is a pointer to parent_type::block.
+        typename blocks_type::value_type blk = *m_pos;
+        if (blk->mp_data)
+            m_cur_node.type = mdds::mtv::get_block_type(*blk->mp_data);
+        else
+            m_cur_node.type = mdds::mtv::element_type_empty;
+
+        m_cur_node.size = blk->m_size;
+        m_cur_node.data = blk->mp_data;
+    }
+
+    node m_cur_node;
+    base_iterator_type m_pos;
+    base_iterator_type m_end;
+};
+
+template<typename _ParentType, typename _BlksType, typename _BaseItrType>
+class iterator_base : public iterator_common_base<_ParentType,_BlksType,_BaseItrType>
+{
+    typedef iterator_common_base<_ParentType,_BlksType,_BaseItrType> common_base;
+    typedef _BaseItrType base_iterator_type;
+
+    using common_base::update_node;
+    using common_base::m_cur_node;
+    using common_base::m_pos;
+    using common_base::m_end;
+
 public:
 
     // iterator traits
-    typedef node value_type;
+    typedef typename common_base::node value_type;
     typedef value_type* pointer;
     typedef value_type& reference;
     typedef ptrdiff_t   difference_type;
@@ -56,18 +102,10 @@ public:
 public:
     iterator_base() {}
     iterator_base(const base_iterator_type& pos, const base_iterator_type& end) :
-        m_pos(pos), m_end(end)
-    {
-        if (m_pos != m_end)
-            update_node();
-    }
+        common_base(pos, end) {}
 
     iterator_base(const iterator_base& other) :
-        m_pos(other.m_pos), m_end(other.m_end)
-    {
-        if (m_pos != m_end)
-            update_node();
-    }
+        common_base(other) {}
 
     bool operator== (const iterator_base& other) const
     {
@@ -126,81 +164,43 @@ public:
      * from iterator_base.
      */
     const base_iterator_type& get_end() const { return m_end; }
-
-private:
-
-    void update_node()
-    {
-        // blocks_type::value_type is a pointer to column_type::block.
-        const typename blocks_type::value_type blk = *m_pos;
-        if (blk->mp_data)
-            m_cur_node.type = mdds::mtv::get_block_type(*blk->mp_data);
-        else
-            m_cur_node.type = mdds::mtv::element_type_empty;
-
-        m_cur_node.size = blk->m_size;
-        m_cur_node.data = blk->mp_data;
-    }
-
-private:
-    node m_cur_node;
-    base_iterator_type m_pos;
-    base_iterator_type m_end;
 };
 
 template<typename _ParentType, typename _BlksType, typename _BaseItrType, typename _NonConstItrBase>
-class const_iterator_base
+class const_iterator_base : public iterator_common_base<_ParentType,_BlksType,_BaseItrType>
 {
-    typedef _ParentType parent_type;
-    typedef _BlksType blocks_type;
+    typedef iterator_common_base<_ParentType,_BlksType,_BaseItrType> common_base;
     typedef _BaseItrType base_iterator_type;
 
-    struct node
-    {
-        mdds::mtv::element_t type;
-        typename parent_type::size_type size;
-        const typename parent_type::element_block_type* data;
-
-        node() : type(mdds::mtv::element_type_empty), size(0), data(NULL) {}
-        node(const node& other) : type(other.type), size(other.size), data(other.data) {}
-    };
+    using common_base::update_node;
+    using common_base::m_cur_node;
+    using common_base::m_pos;
+    using common_base::m_end;
 
 public:
 
     typedef _NonConstItrBase iterator_base;
 
     // iterator traits
-    typedef node value_type;
+    typedef typename common_base::node value_type;
     typedef value_type* pointer;
     typedef value_type& reference;
     typedef ptrdiff_t   difference_type;
     typedef std::bidirectional_iterator_tag iterator_category;
 
 public:
-    const_iterator_base() {}
+    const_iterator_base() : common_base() {}
     const_iterator_base(const base_iterator_type& pos, const base_iterator_type& end) :
-        m_pos(pos), m_end(end)
-    {
-        if (m_pos != m_end)
-            update_node();
-    }
+        common_base(pos, end) {}
 
     const_iterator_base(const const_iterator_base& other) :
-        m_pos(other.m_pos), m_end(other.m_end)
-    {
-        if (m_pos != m_end)
-            update_node();
-    }
+        common_base(other) {}
 
     /**
      * Take the non-const iterator counterpart to create a const iterator.
      */
     const_iterator_base(const iterator_base& other) :
-        m_pos(other.get_pos()), m_end(other.get_end())
-    {
-        if (m_pos != m_end)
-            update_node();
-    }
+        common_base(other.get_pos(), other.get_end()) {}
 
     bool operator== (const const_iterator_base& other) const
     {
@@ -259,26 +259,6 @@ public:
         update_node();
         return &m_cur_node;
     }
-
-private:
-
-    void update_node()
-    {
-        // blocks_type::value_type is a pointer to column_type::block.
-        const typename blocks_type::value_type blk = *m_pos;
-        if (blk->mp_data)
-            m_cur_node.type = mdds::mtv::get_block_type(*blk->mp_data);
-        else
-            m_cur_node.type = mdds::mtv::element_type_empty;
-
-        m_cur_node.size = blk->m_size;
-        m_cur_node.data = blk->mp_data;
-    }
-
-private:
-    node m_cur_node;
-    base_iterator_type m_pos;
-    base_iterator_type m_end;
 };
 
 }}
