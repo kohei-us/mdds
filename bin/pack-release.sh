ab2dcfbd32b8957c997fbd63b9bc48cfc283b175
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-07-06 19:57:47 -0400

    Move all obsolete files into obsolete directory.
    Let's not leave them in the official include and src directories.

--- bin/pack-release.sh
+++ bin/pack-release.sh
@@ -8,6 +8,6 @@ git clone --depth 1 --branch $VERSION https://gitlab.com/mdds/mdds.git $DIR
 pushd .
 cd $DIR
 autoconf
-rm -rf autom4te.cache .git bin slickedit vsprojects include/obsolete
+rm -rf autom4te.cache .git bin slickedit vsprojects obsolete
 popd
 tar jcvf $DIR.tar.bz2 $DIR
