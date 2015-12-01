7eea39441367f26b6d3e3856f0b759f28e404263
David Tardon <dtardon@redhat.com>
2015-12-01 10:18:04 +0100

    mdds#2 switch to automake

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
 
-# Create configure script from configure.ac.
-autoconf || exit 1
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-./configure $@
+olddir=`pwd`
+cd $srcdir
+
+aclocal --version > /dev/null 2> /dev/null || {
+    echo "error: aclocal not found"
+    exit 1
+}
+automake --version > /dev/null 2> /dev/null || {
+    echo "error: automake not found"
+    exit 1
+}
+
+amcheck=`automake --version | grep 'automake (GNU automake) 1.5'`
+if test "x$amcheck" = "xautomake (GNU automake) 1.5"; then
+    echo "warning: you appear to be using automake 1.5"
+    echo "         this version has a bug - GNUmakefile.am dependencies are not generated"
+fi
+
+rm -rf autom4te*.cache
+
+aclocal $ACLOCAL_FLAGS || {
+    echo "error: aclocal $ACLOCAL_FLAGS failed"
+    exit 1
+}
+automake -a -c --foreign || {
+    echo "warning: automake failed"
+}
+autoconf || {
+    echo "error: autoconf failed"
+    exit 1
+}
