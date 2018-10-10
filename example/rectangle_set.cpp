0a6ec3cf02af8a594bad6770427e5b45cc842337
Kohei Yoshida <kohei.yoshida@gmail.com>
2018-10-10 18:28:01 -0400

    Fix some of the example codes.

--- example/rectangle_set.cpp
+++ example/rectangle_set.cpp
@@ -9,9 +9,9 @@ typedef ::mdds::rectangle_set<long, string> rectset_type;
 
 struct string_printer : public unary_function<string*, void>
 {
-    void operator() (const string* p) const
+    void operator() (const string& s) const
     {
-        cout << "search hit: " << *p << endl;
+        cout << "search hit: " << s << endl;
     }
 };
 
@@ -23,9 +23,9 @@ int main()
     string C("C");
 
     // Insert data into the tree.
-    db.insert(0, 0, 12, 11, &A);
-    db.insert(2, 5, 20, 25, &B);
-    db.insert(10, 15, 40, 45, &C);
+    db.insert(0, 0, 12, 11, A);
+    db.insert(2, 5, 20, 25, B);
+    db.insert(10, 15, 40, 45, C);
 
     // Run search and get the result.
     rectset_type::search_result result = db.search(5, 10);
--- example/segment_tree.cpp
+++ example/segment_tree.cpp
@@ -9,9 +9,9 @@ typedef ::mdds::segment_tree<long, string> db_type;
 
 struct string_printer : public unary_function<string*, void>
 {
-    void operator() (const string* p) const
+    void operator() (const string& s) const
     {
-        cout << "search hit: " << *p << endl;
+        cout << "search hit: " << s << endl;
     }
 };
 
@@ -23,9 +23,9 @@ int main()
     string C("C");
 
     // Insert data into the tree.
-    db.insert(0,  10, &A);
-    db.insert(2,  20, &B);
-    db.insert(10, 15, &C);
+    db.insert(0,  10, A);
+    db.insert(2,  20, B);
+    db.insert(10, 15, C);
 
     // Don't forget to build it before calling search().
     db.build_tree();
