fcfcaacc305a6a479b77ed1842b33e83215b0c77
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-08-08 08:28:25 -0400

    This is year 2019.

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'mdds'
-copyright = '2018, Kohei Yoshida'
+copyright = '2019, Kohei Yoshida'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
