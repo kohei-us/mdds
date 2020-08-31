75a342952234647aab4cd6da42198fc3a082b0b6
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-08-31 19:19:49 -0400

    Also print the header part & fix the wrong URLs.

--- bin/gen-gfm-release-table.sh
+++ bin/gen-gfm-release-table.sh
@@ -7,11 +7,18 @@ VER=$(cat ./configure.ac | grep AC_INIT | sed -e "s/.*\([0-9][0-9]*\.[0-9][0-9]*
 
 PKGS=$(ls $PKG_PREFIX-$VER.tar.*)
 
+echo "## Release Notes"
+echo ""
+echo "* add item"
+echo ""
+echo "## Source packages for autotools"
+echo ""
+
 echo "| URL | sha256sum | size |"
 echo "|-----|-----------|------|"
 
 for _PKG in $PKGS; do
-    _URL="[$_PKG](https://kohei.us/files/orcus/src/$_PKG)"
+    _URL="[$_PKG](https://kohei.us/files/$PKG_PREFIX/src/$_PKG)"
     _HASH=$(sha256sum $_PKG | sed -e "s/^\(.*\)$PKG_PREFIX.*/\1/g" | tr -d "[:space:]")
     _SIZE=$(stat -c "%s" $_PKG)
     echo "| $_URL | $_HASH | $_SIZE |"
