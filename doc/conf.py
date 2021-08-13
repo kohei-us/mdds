17e13babe579b71cf81440379a08916d45735044
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-08-13 13:20:58 -0400

    Try printing doxygen version.

--- doc/conf.py
+++ doc/conf.py
@@ -20,7 +20,7 @@ import subprocess
 rtd_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if rtd_build:
-    subprocess.call("doxygen doxygen.conf", shell=True)
+    subprocess.call("doxygen --version; doxygen doxygen.conf", shell=True)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
