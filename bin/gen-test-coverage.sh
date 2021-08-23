bb7269d1e7cd964b6ba7e61e0bac71dfe352368f
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-08-23 11:28:41 -0400

    Fix the test coverage script to adopt to the new test program locations.
    Turns out I don't need to run gcov at all; lcov can recursively scan
    the directory for the gcda files.

--- bin/gen-test-coverage.sh
+++ bin/gen-test-coverage.sh
@@ -1,33 +1,22 @@
 #!/usr/bin/env bash
 
 COVFILE="coverage.info"
-ROOTDIR="$PWD"
+PROGDIR=$(dirname $0)
+TESTDIR=$(realpath "$PROGDIR/../test")
 
 # Get the output directory from the user.
 OUTDIR=$1
 shift
 
 if [ -z "$OUTDIR" ]; then
-    echo "Usage: $0 [output directory] file1 file2 file3 ..."
+    echo "Usage: $0 [output directory]"
     exit 1
 fi
 
-# Turn the file paths into absolute path.
-_FILES=
-for _FILE in "$@"; do
-    _FILES="$_FILES $ROOTDIR/$_FILE"
-done
+mkdir -p "$OUTDIR"
+cd "$OUTDIR"
 
-mkdir -p $OUTDIR
-cd $OUTDIR
-gcov $_FILES || exit 1
-
-# For now, we assume that all test source files are located in the same directory.
-SRCDIR=$(echo $_FILES | cut -d' ' -f1)
-SRCDIR=$(dirname $SRCDIR)
-
-# Generate HTML reports.
-lcov --capture --directory $SRCDIR -o $COVFILE || exit 1
+lcov --capture --directory "$TESTDIR" -o $COVFILE || exit 1
 genhtml $COVFILE -o html || exit 1
 
 # View the reports in web browser.
