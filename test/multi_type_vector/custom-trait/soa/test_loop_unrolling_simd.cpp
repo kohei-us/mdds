e18a88e861a2e7d9b869d1280f71e71d7e752de3
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-08-28 13:18:43 -0400

    Remove almost duplicate code blocks.

--- test/multi_type_vector/custom-trait/soa/test_loop_unrolling_simd.cpp
+++ test/multi_type_vector/custom-trait/soa/test_loop_unrolling_simd.cpp
@@ -41,6 +41,25 @@ struct trait_lu
     constexpr static lu_factor_t loop_unrolling = F;
 };
 
+template<lu_factor_t F>
+void run_test()
+{
+    using mtv_type = mtv_alias_type<trait_lu<F>>;
+
+    mtv_type db(5, std::string("test"));
+
+    // keep inserting blocks of alternating types at position 0 to force block
+    // position adjustments.
+
+    for (int i = 0; i < 50; ++i)
+    {
+        db.insert_empty(0, 2);
+
+        std::vector<int8_t> values(2, 89);
+        db.insert(0, values.begin(), values.end());
+    }
+}
+
 }
 
 #if SIZEOF_VOID_P == 8 && defined(__SSE2__)
@@ -49,40 +68,14 @@ void mtv_test_loop_unrolling_sse2_x64()
 {
     stack_printer __stack_printer__(__FUNCTION__);
 
-    using mtv_type = mtv_alias_type<trait_lu<lu_factor_t::sse2_x64>>;
-
-    mtv_type db(5, std::string("test"));
-
-    // keep inserting blocks of alternating types at position 0 to force block
-    // position adjustments.
-
-    for (int i = 0; i < 50; ++i)
-    {
-        db.insert_empty(0, 2);
-
-        std::vector<int8_t> values(2, 89);
-        db.insert(0, values.begin(), values.end());
-    }
+    run_test<lu_factor_t::sse2_x64>();
 }
 
 void mtv_test_loop_unrolling_sse2_x64_4()
 {
     stack_printer __stack_printer__(__FUNCTION__);
 
-    using mtv_type = mtv_alias_type<trait_lu<lu_factor_t::sse2_x64_lu4>>;
-
-    mtv_type db(5, std::string("test"));
-
-    // keep inserting blocks of alternating types at position 0 to force block
-    // position adjustments.
-
-    for (int i = 0; i < 50; ++i)
-    {
-        db.insert_empty(0, 2);
-
-        std::vector<int8_t> values(2, 89);
-        db.insert(0, values.begin(), values.end());
-    }
+    run_test<lu_factor_t::sse2_x64_lu4>();
 }
 
 #else
