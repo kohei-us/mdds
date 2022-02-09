c01e4a4944f5c4264df62b147b54b8331fd05a56
Kohei Yoshida <kohei.yoshida@gmail.com>
2022-02-08 22:03:38 -0500

    Add workspace pre-processing defs for slickedit.

---
+++ slickedit/mdds_cpp.h
@@ -0,0 +1,4 @@
+#define MDDS_MTV_TRACE(method_type) ::mdds::detail::mtv::call_trace<Trait>{}({trace_method_t::method_type,this,__func__,"",__FILE__,__LINE__})
+
+#define MDDS_MTV_TRACE_ARGS(method_type, stream) do{std::ostringstream _os_;_os_<<stream;::mdds::detail::mtv::call_trace<Trait>{}({trace_method_t::method_type,this,__func__,_os_.str(),__FILE__,__LINE__});}while (false)
+
