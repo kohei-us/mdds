e3c57eb66dd99b443f190500148bea0937bd0b64
Kohei Yoshida <kohei.yoshida@gmail.com>
2022-08-07 13:53:33 -0400

    Try adding dh-autoreconf package...

--- autogen.sh
+++ autogen.sh
@@ -6,10 +6,6 @@ test -z "$srcdir" && srcdir=.
 olddir=`pwd`
 cd $srcdir
 
-automake -a -c --foreign || {
-    echo "warning: automake failed"
-}
-
 autoreconf || {
     echo "error: autoreconf failed"
     exit 1
