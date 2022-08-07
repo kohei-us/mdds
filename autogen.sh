f346ae7bcb4a782ded153634f89c82b67400e496
Kohei Yoshida <kohei.yoshida@gmail.com>
2022-08-07 13:48:33 -0400

    Add back the automake command

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,10 @@ test -z "$srcdir" && srcdir=.
 olddir=`pwd`
 cd $srcdir
 
+automake -a -c --foreign || {
+    echo "warning: automake failed"
+}
+
 autoreconf || {
     echo "error: autoreconf failed"
     exit 1
