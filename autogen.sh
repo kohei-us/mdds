a155648609a7554191fe5a8f03f8b1fdab4fa903
Kohei Yoshida <kohei.yoshida@gmail.com>
2022-08-07 12:48:59 -0400

    Just use autoreconf which should know what low-level commands to call
    c.f. https://stackoverflow.com/questions/27285052/difference-between-autoconf-and-autoreconf

--- autogen.sh
+++ autogen.sh
@@ -6,37 +6,8 @@ test -z "$srcdir" && srcdir=.
 olddir=`pwd`
 cd $srcdir
 
-aclocal --version > /dev/null 2> /dev/null || {
-    echo "error: aclocal not found"
-    exit 1
-}
-automake --version > /dev/null 2> /dev/null || {
-    echo "error: automake not found"
-    exit 1
-}
-
-amcheck=`automake --version | grep 'automake (GNU automake) 1.5'`
-if test "x$amcheck" = "xautomake (GNU automake) 1.5"; then
-    echo "warning: you appear to be using automake 1.5"
-    echo "         this version has a bug - GNUmakefile.am dependencies are not generated"
-fi
-
-rm -rf autom4te*.cache
-
-libtoolize --force --copy || {
-    echo "error: libtoolize failed"
-    exit 1
-}
-
-aclocal $ACLOCAL_FLAGS || {
-    echo "error: aclocal $ACLOCAL_FLAGS failed"
-    exit 1
-}
-automake -a -c --foreign || {
-    echo "warning: automake failed"
-}
-autoconf || {
-    echo "error: autoconf failed"
+autoreconf || {
+    echo "error: autoreconf failed"
     exit 1
 }
 
