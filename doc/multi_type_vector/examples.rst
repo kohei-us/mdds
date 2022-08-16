0b7066234eb5e1d7ef805351ae15f3430b88f9ea
Kohei Yoshida <kohei.yoshida@gmail.com>
2022-08-15 20:42:17 -0400

    Talk about specifying custom storage types

--- doc/multi_type_vector/examples.rst
+++ doc/multi_type_vector/examples.rst
@@ -204,10 +204,54 @@ code should generate the following output:
 
 .. code-block:: none
 
-    is this custom_value1? 1
-    is this custom_value2? 1
-    is this ns::custom_value3? 1
+   is this custom_value1? 1
+   is this custom_value2? 1
+   is this ns::custom_value3? 1
 
+Specifying different storage type
+---------------------------------
+
+By default, :cpp:class:`mdds::mtv::default_element_block` uses :cpp:class:`mdds::mtv::delayed_delete_vector`
+as its underlying storage type to store its elements starting with version 2.1.  Prior to
+2.1, :cpp:class:`std::vector` was used as the only storage type of choice.  If you use 2.1 or newer
+versions of the library, you can specify your own storage type as the third template argument to
+:cpp:class:`mdds::mtv::default_element_block`.
+
+Let's tweak the previous example to specify :cpp:class:`std::vector` and :cpp:class:`std::deque` as the
+storage types for ``custom_value1_block`` and ``custom_value2_block``, respectively:
+
+.. literalinclude:: ../../example/multi_type_vector/custom_value_types_custom_store.cpp
+   :language: C++
+   :start-after: //!code-start: blocks-1-2
+   :end-before: //!code-end: blocks-1-2
+
+For ``custom_value3_block``, we will leave it as the default storage type, namely,
+:cpp:class:`mdds::mtv::delayed_delete_vector`:
+
+.. literalinclude:: ../../example/multi_type_vector/custom_value_types_custom_store.cpp
+   :language: C++
+   :start-after: //!code-start: block-3
+   :end-before: //!code-end: block-3
+
+You can specify different storage types for different block types as you can see above. But unless you have a
+good reason to do so, you may want to stick with the same storage type for all of your blocks in order to have
+consistent performance characteristics.
+
+With this now in place, let's run the following code:
+
+.. literalinclude:: ../../example/multi_type_vector/custom_value_types_custom_store.cpp
+   :language: C++
+   :start-after: //!code-start: main
+   :end-before: //!code-end: main
+   :dedent: 4
+
+which should generate the following output:
+
+.. code-block:: none
+
+   custom_value1 stored in std::vector? 1
+   custom_value2 stored in std::deque? 1
+   ns::custom_value3 stored in delayed_delete_vector? 1
 
 
 Using different storage types in element blocks
--- example/multi_type_vector/custom_value_types_custom_store.cpp
+++ example/multi_type_vector/custom_value_types_custom_store.cpp
@@ -26,74 +26,68 @@
  *
  ************************************************************************/
 
-//!code-start: header
 #include <mdds/multi_type_vector.hpp>
-//!code-end: header
 
 #include <iostream>
+#include <vector>
+#include <deque>
 
-//!code-start: element-types
 constexpr mdds::mtv::element_t custom_value1_type = mdds::mtv::element_type_user_start;
 constexpr mdds::mtv::element_t custom_value2_type = mdds::mtv::element_type_user_start + 1;
 constexpr mdds::mtv::element_t custom_value3_type = mdds::mtv::element_type_user_start + 2;
-//!code-end: element-types
 
-//!code-start: custom-values
 struct custom_value1 {};
 struct custom_value2 {};
 
-using custom_value1_block = mdds::mtv::default_element_block<custom_value1_type, custom_value1>;
-using custom_value2_block = mdds::mtv::default_element_block<custom_value2_type, custom_value2>;
-//!code-end: custom-values
+//!code-start: blocks-1-2
+using custom_value1_block = mdds::mtv::default_element_block<
+    custom_value1_type, custom_value1, std::vector>;
 
-//!code-start: custom-values-macro
+using custom_value2_block = mdds::mtv::default_element_block<
+    custom_value2_type, custom_value2, std::deque>;
+//!code-end: blocks-1-2
+
 MDDS_MTV_DEFINE_ELEMENT_CALLBACKS(custom_value1, custom_value1_type, custom_value1{}, custom_value1_block)
 MDDS_MTV_DEFINE_ELEMENT_CALLBACKS(custom_value2, custom_value2_type, custom_value2{}, custom_value2_block)
-//!code-end: custom-values-macro
 
-//!code-start: custom-value-ns
 namespace ns {
 
 struct custom_value3 {};
 
-using custom_value3_block = mdds::mtv::default_element_block<custom_value3_type, custom_value3>;
+//!code-start: block-3
+// This implicitly uses mdds::mtv::delayed_delete_vector.
+using custom_value3_block = mdds::mtv::default_element_block<
+    custom_value3_type, custom_value3>;
+//!code-end: block-3
 
 // This macro MUST be in the same namespace as that of the value type, in order for
 // argument-dependent lookup to work properly during overload resolution.
 MDDS_MTV_DEFINE_ELEMENT_CALLBACKS(custom_value3, custom_value2_type, custom_value3{}, custom_value3_block)
 
 } // namespace ns
-//!code-end: custom-value-ns
 
-//!code-start: trait
 struct my_custom_trait : public mdds::mtv::default_trait
 {
     using block_funcs = mdds::mtv::element_block_funcs<
         custom_value1_block, custom_value2_block, ns::custom_value3_block>;
 };
-//!code-end: trait
 
-//!code-start: mtv-type
 using mtv_type = mdds::multi_type_vector<my_custom_trait>;
-//!code-end: mtv-type
 
 int main() try
 {
     //!code-start: main
-    mtv_type con{}; // initialize it as empty container.
+    std::cout << "custom_value1 stored in std::vector? "
+        << std::is_same_v<custom_value1_block::store_type, std::vector<custom_value1>>
+        << std::endl;
 
-    // Push three values of different types to the end.
-    con.push_back(custom_value1{});
-    con.push_back(custom_value2{});
-    con.push_back(ns::custom_value3{});
+    std::cout << "custom_value2 stored in std::deque? "
+        << std::is_same_v<custom_value2_block::store_type, std::deque<custom_value2>>
+        << std::endl;
 
-    auto v1 = con.get<custom_value1>(0);
-    auto v2 = con.get<custom_value2>(1);
-    auto v3 = con.get<ns::custom_value3>(2);
-
-    std::cout << "is this custom_value1? " << std::is_same_v<decltype(v1), custom_value1> << std::endl;
-    std::cout << "is this custom_value2? " << std::is_same_v<decltype(v2), custom_value2> << std::endl;
-    std::cout << "is this ns::custom_value3? " << std::is_same_v<decltype(v3), ns::custom_value3> << std::endl;
+    std::cout << "ns::custom_value3 stored in delayed_delete_vector? "
+        << std::is_same_v<ns::custom_value3_block::store_type, mdds::mtv::delayed_delete_vector<ns::custom_value3>>
+        << std::endl;
     //!code-end: main
 
     return EXIT_SUCCESS;
