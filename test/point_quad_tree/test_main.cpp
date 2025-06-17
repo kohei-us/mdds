19943f78689a23f526920412b3405816c9b4c1c3
Kohei Yoshida <kohei.yoshida@gmail.com>
2025-06-16 21:39:53 -0400

    Replace assert with TEST_ASSERT in point_quad_tree tests
    Also use MDDS_TEST_FUNC_SCOPE and remove 'using namespace std'.

--- test/point_quad_tree/test_main.cpp
+++ test/point_quad_tree/test_main.cpp
@@ -26,7 +26,8 @@
  ************************************************************************/
 
 #include "test_global.hpp" // This must be the first header to be included.
-#include "mdds/point_quad_tree.hpp"
+
+#include <mdds/point_quad_tree.hpp>
 
 #include <algorithm>
 #include <memory>
@@ -34,13 +35,12 @@
 #include <sstream>
 #include <boost/cstdint.hpp>
 
-using namespace std;
 using namespace mdds;
 using ::boost::uint16_t;
 
 struct data_printer
 {
-    void operator()(const string* p)
+    void operator()(const std::string* p)
     {
         cout << *p << " ";
     }
@@ -49,7 +49,7 @@ struct data_printer
 template<typename _DbType>
 struct search_result_printer
 {
-    void operator()(const pair<const typename _DbType::point, const typename _DbType::value_type>& r) const
+    void operator()(const std::pair<const typename _DbType::point, const typename _DbType::value_type>& r) const
     {
         cout << "  (x=" << r.first.x << ", y=" << r.first.y << ", value='" << *r.second << "')" << endl;
     }
@@ -57,24 +57,25 @@ struct search_result_printer
 
 void pqt_test_basic()
 {
-    stack_printer __stack_printer__("::pqt_test");
-    typedef point_quad_tree<uint16_t, const string*> db_type;
+    MDDS_TEST_FUNC_SCOPE;
+
+    typedef point_quad_tree<uint16_t, const std::string*> db_type;
     db_type db;
 
-    string A("A");
-    string B("B");
-    string C("C");
-    string D("D");
-    string E("E");
-    string F("F");
-    string G("G");
-    string H("H");
-    string I("I");
-    string J("J");
-    string K("K");
-    string L("L");
-    string M("M");
-    string N("N");
+    std::string A("A");
+    std::string B("B");
+    std::string C("C");
+    std::string D("D");
+    std::string E("E");
+    std::string F("F");
+    std::string G("G");
+    std::string H("H");
+    std::string I("I");
+    std::string J("J");
+    std::string K("K");
+    std::string L("L");
+    std::string M("M");
+    std::string N("N");
 
     db.insert(25, 32, &A);
     db.insert(5, 45, &B);
@@ -91,7 +92,7 @@ void pqt_test_basic()
     db.insert(39, 78, &M);
     db.insert(72, 52, &N);
 
-    assert(db.size() == 14);
+    TEST_ASSERT(db.size() == 14);
 
     cout << "node count = " << get_node_instance_count() << endl;
     db.dump_tree_svg("./obj/test.svg");
@@ -124,35 +125,36 @@ void pqt_test_basic()
 
     db.remove(20, 20);
     db.remove(40, 50);
-    assert(db.size() == 12);
+    TEST_ASSERT(db.size() == 12);
     db.dump_tree_svg("./obj/test-remove.svg");
 
     db.clear();
-    assert(db.empty());
-    assert(db.size() == 0);
+    TEST_ASSERT(db.empty());
+    TEST_ASSERT(db.size() == 0);
 }
 
 void pqt_test_insertion_removal()
 {
-    stack_printer __stack_printer__("::pqt_test_insertion_removal");
-    typedef point_quad_tree<int32_t, const string*> db_type;
+    MDDS_TEST_FUNC_SCOPE;
+
+    typedef point_quad_tree<int32_t, const std::string*> db_type;
     db_type db;
 
     // Check its empty-ness...
-    assert(db.empty());
-    assert(db.size() == 0);
+    TEST_ASSERT(db.empty());
+    TEST_ASSERT(db.size() == 0);
 
     // Create all data instances.
-    vector<unique_ptr<string>> data_store;
+    std::vector<std::unique_ptr<std::string>> data_store;
     data_store.reserve(100);
     for (size_t i = 0; i < 100; ++i)
     {
-        ostringstream os;
-        os << "0x" << hex << i;
-        data_store.emplace_back(new string(os.str()));
+        std::ostringstream os;
+        os << "0x" << std::hex << i;
+        data_store.emplace_back(new std::string(os.str()));
     }
 
-    vector<db_type::node_data> expected;
+    std::vector<db_type::node_data> expected;
 
     // Insert data one by one, and verify each insertion.
     for (int32_t i = 0; i < 10; ++i)
@@ -161,18 +163,18 @@ void pqt_test_insertion_removal()
         {
             int32_t x = i * 10 + 1, y = j * 10 + 1;
             size_t index = i * 10 + j;
-            const string* data_ptr = data_store[index].get();
+            const std::string* data_ptr = data_store[index].get();
             cout << "inserting '" << *data_ptr << "' at (" << x << "," << y << ")" << endl;
             db.insert(x, y, data_ptr);
             expected.push_back(db_type::node_data(x, y, data_ptr));
 
-            vector<db_type::node_data> stored_data;
+            std::vector<db_type::node_data> stored_data;
             db.get_all_stored_data(stored_data);
-            assert(stored_data.size() == (index + 1));
-            assert(db.size() == (index + 1));
-            assert(!db.empty());
+            TEST_ASSERT(stored_data.size() == (index + 1));
+            TEST_ASSERT(db.size() == (index + 1));
+            TEST_ASSERT(!db.empty());
             bool success = db.verify_data(expected);
-            assert(success);
+            TEST_ASSERT(success);
         }
     }
     db.dump_tree_svg("./obj/pqt_test_insertion.svg");
@@ -189,20 +191,21 @@ void pqt_test_insertion_removal()
             cout << "removing node at (" << x << "," << y << ")  "
                  << "size after removal: " << n << endl;
             --node_count;
-            assert(node_count == n);
+            TEST_ASSERT(node_count == n);
         }
     }
 }
 
 void pqt_test_remove_root()
 {
-    stack_printer __stack_printer__("::pqt_test_remove_root");
-    typedef point_quad_tree<int32_t, const string*> db_type;
-    string O("O");
-    string NW("NW");
-    string NE("NE");
-    string SW("SW");
-    string SE("SE");
+    MDDS_TEST_FUNC_SCOPE;
+
+    typedef point_quad_tree<int32_t, const std::string*> db_type;
+    std::string O("O");
+    std::string NW("NW");
+    std::string NE("NE");
+    std::string SW("SW");
+    std::string SE("SE");
     db_type db;
 
     // Insert all data and verify their storage.
@@ -213,15 +216,15 @@ void pqt_test_remove_root()
     db.insert(0, 20, &SW);
     db.dump_tree_svg("./obj/pqt_test_remove_root-1.svg");
 
-    vector<db_type::node_data> expected;
+    std::vector<db_type::node_data> expected;
     expected.push_back(db_type::node_data(10, 10, &O));
     expected.push_back(db_type::node_data(20, 0, &NE));
     expected.push_back(db_type::node_data(0, 0, &NW));
     expected.push_back(db_type::node_data(20, 20, &SE));
     expected.push_back(db_type::node_data(0, 20, &SW));
     bool success = db.verify_data(expected);
-    assert(success);
-    assert(db.size() == 5);
+    TEST_ASSERT(success);
+    TEST_ASSERT(db.size() == 5);
 
     // Now, remove the root node.
     db.remove(10, 10);
@@ -232,41 +235,41 @@ void pqt_test_remove_root()
     expected.push_back(db_type::node_data(20, 20, &SE));
     expected.push_back(db_type::node_data(0, 20, &SW));
     success = db.verify_data(expected);
-    assert(success);
-    assert(db.size() == 4);
+    TEST_ASSERT(success);
+    TEST_ASSERT(db.size() == 4);
 }
 
 void pqt_test_equality()
 {
-    stack_printer __stack_printer__("::pqt_test_equality");
+    MDDS_TEST_FUNC_SCOPE;
 
-    typedef point_quad_tree<int32_t, const string*> db_type;
+    typedef point_quad_tree<int32_t, const std::string*> db_type;
     db_type db1, db2;
 
-    string A("A");
-    string B("B");
-    string C("C");
-    string D("D");
-    string E("E");
-    string F("F");
+    std::string A("A");
+    std::string B("B");
+    std::string C("C");
+    std::string D("D");
+    std::string E("E");
+    std::string F("F");
 
-    assert(db1 == db2); // both are empty.
+    TEST_ASSERT(db1 == db2); // both are empty.
 
     db1.insert(0, 0, &A);
     db2.insert(0, 0, &A);
-    assert(db1 == db2);
+    TEST_ASSERT(db1 == db2);
     db1.remove(0, 0);
-    assert(db1 != db2);
+    TEST_ASSERT(db1 != db2);
     db1.insert(0, 0, &B);
-    assert(db1 != db2);
+    TEST_ASSERT(db1 != db2);
     db2.insert(0, 0, &B); // B overwrites A.
-    assert(db1 == db2); // Both should have B at (0,0).
+    TEST_ASSERT(db1 == db2); // Both should have B at (0,0).
     db1.insert(1, 1, &C);
     db2.insert(2, 2, &C);
-    assert(db1 != db2);
+    TEST_ASSERT(db1 != db2);
     db1.insert(2, 2, &C);
     db2.insert(1, 1, &C);
-    assert(db1 == db2);
+    TEST_ASSERT(db1 == db2);
 
     // Inserting data in different orders should make no difference in equality.
     db1.insert(1, 3, &D);
@@ -276,86 +279,88 @@ void pqt_test_equality()
     db2.insert(1, 5, &F);
     db2.insert(1, 4, &E);
     db2.insert(1, 3, &D);
-    assert(db1 == db2);
+    TEST_ASSERT(db1 == db2);
     db1.remove(1, 4);
     db2.remove(1, 4);
-    assert(db1 == db2);
+    TEST_ASSERT(db1 == db2);
 
     // Make them empty again.
     db1.clear();
     db2.clear();
-    assert(db1 == db2);
+    TEST_ASSERT(db1 == db2);
 }
 
 void pqt_test_assignment()
 {
-    stack_printer __stack_printer__("::pqt_test_assignment");
-    typedef point_quad_tree<int32_t, const string*> db_type;
+    MDDS_TEST_FUNC_SCOPE;
+
+    typedef point_quad_tree<int32_t, const std::string*> db_type;
     db_type db1, db2;
-    string A("A");
-    string B("B");
-    string C("C");
-    string D("D");
-    string E("E");
-    string F("F");
+    std::string A("A");
+    std::string B("B");
+    std::string C("C");
+    std::string D("D");
+    std::string E("E");
+    std::string F("F");
 
     db1.insert(0, 10, &A);
     db1.insert(2, 5, &B);
     db1.insert(-10, 2, &C);
     db1.insert(5, 7, &D);
-    vector<db_type::node_data> expected;
+    std::vector<db_type::node_data> expected;
     expected.push_back(db_type::node_data(0, 10, &A));
     expected.push_back(db_type::node_data(2, 5, &B));
     expected.push_back(db_type::node_data(-10, 2, &C));
     expected.push_back(db_type::node_data(5, 7, &D));
     bool success = db1.verify_data(expected);
-    assert(success);
+    TEST_ASSERT(success);
 
     db2 = db1;
     success = db2.verify_data(expected);
-    assert(success);
+    TEST_ASSERT(success);
     success = db1.verify_data(expected);
-    assert(success);
+    TEST_ASSERT(success);
 
     db2.insert(12, 45, &E);
     db2.insert(20, 42, &F);
     success = db2.verify_data(expected); // This should fail.
-    assert(!success);
+    TEST_ASSERT(!success);
     db2 = db1; // Assign once again.
     success = db2.verify_data(expected); // This now should succeed.
-    assert(success);
+    TEST_ASSERT(success);
 }
 
 void pqt_test_swap()
 {
-    stack_printer __stack_printer__("::pqt_test_swap");
-    typedef point_quad_tree<int32_t, const string*> db_type;
+    MDDS_TEST_FUNC_SCOPE;
+
+    typedef point_quad_tree<int32_t, const std::string*> db_type;
     db_type db1, db2;
-    string A("A");
-    string B("B");
-    string C("C");
-    string D("D");
-    string E("E");
-    string F("F");
+    std::string A("A");
+    std::string B("B");
+    std::string C("C");
+    std::string D("D");
+    std::string E("E");
+    std::string F("F");
 
     db1.insert(0, 10, &A);
     db1.insert(2, 5, &B);
     db1.insert(-10, 2, &C);
     db1.insert(5, 7, &D);
-    vector<db_type::node_data> expected;
+    std::vector<db_type::node_data> expected;
     expected.push_back(db_type::node_data(0, 10, &A));
     expected.push_back(db_type::node_data(2, 5, &B));
     expected.push_back(db_type::node_data(-10, 2, &C));
     expected.push_back(db_type::node_data(5, 7, &D));
     bool success = db1.verify_data(expected);
-    assert(success);
-    assert(db2.empty());
+    TEST_ASSERT(success);
+    TEST_ASSERT(db2.empty());
 
     db1.swap(db2);
-    assert(db1.empty());
-    assert(!db2.empty());
+    TEST_ASSERT(db1.empty());
+    TEST_ASSERT(!db2.empty());
     success = db2.verify_data(expected);
-    assert(success);
+    TEST_ASSERT(success);
 }
 
 template<typename _DbType>
@@ -379,77 +384,79 @@ bool verify_find(
 
 void pqt_test_find()
 {
-    stack_printer __stack_printer__("::pqt_test_find");
-    typedef point_quad_tree<int32_t, const string*> db_type;
+    MDDS_TEST_FUNC_SCOPE;
+
+    typedef point_quad_tree<int32_t, const std::string*> db_type;
     db_type db;
-    string A("A");
-    string B("B");
-    string C("C");
-    string D("D");
-    string E("E");
-    string F("F");
+    std::string A("A");
+    std::string B("B");
+    std::string C("C");
+    std::string D("D");
+    std::string E("E");
+    std::string F("F");
     db.insert(92, 27, &A);
     db.insert(53, 26, &B);
     db.insert(69, 18, &C);
     db.insert(0, 78, &D);
     db.insert(17, 7, &E);
     db.insert(91, 88, &F);
-    assert(db.size() == 6);
+    TEST_ASSERT(db.size() == 6);
     db.dump_tree_svg("obj/pqt_test_find.svg");
 
     bool check;
     check = verify_find(db, 92, 27, &A);
-    assert(check);
+    TEST_ASSERT(check);
     check = verify_find(db, 53, 26, &B);
-    assert(check);
+    TEST_ASSERT(check);
     check = verify_find(db, 69, 18, &C);
-    assert(check);
+    TEST_ASSERT(check);
     check = verify_find(db, 0, 78, &D);
-    assert(check);
+    TEST_ASSERT(check);
     check = verify_find(db, 17, 7, &E);
-    assert(check);
+    TEST_ASSERT(check);
     check = verify_find(db, 91, 88, &F);
-    assert(check);
+    TEST_ASSERT(check);
 
     // Check for non-existent data.
     check = verify_find(db, 34, 86, &A);
-    assert(!check);
+    TEST_ASSERT(!check);
     check = verify_find(db, -1, 7, &A);
-    assert(!check);
+    TEST_ASSERT(!check);
     check = verify_find(db, 91, 27, &A);
-    assert(!check);
+    TEST_ASSERT(!check);
 }
 
 void pqt_test_node_access()
 {
-    stack_printer __stack_printer__("::pqt_test_node_access");
-    typedef point_quad_tree<int32_t, const string*> db_type;
+    MDDS_TEST_FUNC_SCOPE;
+
+    typedef point_quad_tree<int32_t, const std::string*> db_type;
     db_type db;
     db_type::node_access nac = db.get_node_access();
-    assert(!nac);
-    string A("A");
-    string B("B");
-    string C("C");
-    string D("D");
-    string E("E");
-    string F("F");
+    TEST_ASSERT(!nac);
+    std::string A("A");
+    std::string B("B");
+    std::string C("C");
+    std::string D("D");
+    std::string E("E");
+    std::string F("F");
     db.insert(92, 27, &A);
     db.insert(53, 26, &B);
     db.insert(69, 18, &C);
     db.insert(0, 78, &D);
     db.insert(17, 7, &E);
     db.insert(91, 88, &F);
-    assert(db.size() == 6);
+    TEST_ASSERT(db.size() == 6);
 
     nac = db.get_node_access();
     // Test root node.
-    assert(nac);
-    assert(nac.x() == 92);
-    assert(nac.y() == 27);
-    assert(nac.data() == &A);
+    TEST_ASSERT(nac);
+    TEST_ASSERT(nac.x() == 92);
+    TEST_ASSERT(nac.y() == 27);
+    TEST_ASSERT(nac.data() == &A);
 
     bool success = db.verify_node_iterator(nac);
-    assert(success);
+    TEST_ASSERT(success);
 }
 
 int main()
@@ -464,7 +471,7 @@ int main()
         pqt_test_swap();
         pqt_test_find();
         pqt_test_node_access();
-        assert(get_node_instance_count() == 0);
+        TEST_ASSERT(get_node_instance_count() == 0);
     }
     catch (const std::exception& e)
     {
