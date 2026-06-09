2c4e260c417591ec13dada83d478045d62ef7e9f
Kohei Yoshida <kohei.yoshida@collabora.com>
2026-06-09 19:51:51 -0400

    Standardize on double quotes & switch to sphinx-rtd-theme

--- doc/conf.py
+++ doc/conf.py
@@ -10,52 +10,52 @@ import sys
 import os
 import subprocess
 
-rtd_build = os.environ.get('READTHEDOCS', None) == 'True'
+rtd_build = os.environ.get("READTHEDOCS", None) == "True"
 
 if rtd_build:
     subprocess.call("doxygen --version; doxygen doxygen.conf", shell=True)
 
-extensions = ['breathe']
+extensions = ["breathe"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-source_suffix = '.rst'
-master_doc = 'index'
+source_suffix = ".rst"
+master_doc = "index"
 
-project = 'mdds'
-copyright = '2025, Kohei Yoshida'
+project = "mdds"
+copyright = "2025, Kohei Yoshida"
 
 version = "4.0"
 release = "4.0.0"
 
-exclude_patterns = ['_build']
+exclude_patterns = ["_build"]
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
-html_theme = 'piccolo_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {}
-html_static_path = ['_static']
-htmlhelp_basename = 'mddsdoc'
+html_static_path = ["_static"]
+htmlhelp_basename = "mddsdoc"
 
 
 latex_elements = {}
 
 latex_documents = [
-  ('index', 'mdds.tex', 'mdds Documentation',
-   'Kohei Yoshida', 'manual'),
+  ("index", "mdds.tex", "mdds Documentation",
+   "Kohei Yoshida", "manual"),
 ]
 
 man_pages = [
-    ('index', 'mdds', 'mdds Documentation',
-     ['Kohei Yoshida'], 1)
+    ("index", "mdds", "mdds Documentation",
+     ["Kohei Yoshida"], 1)
 ]
 
 texinfo_documents = [
-  ('index', 'mdds', 'mdds Documentation',
-   'Kohei Yoshida', 'mdds', 'One line description of project.',
-   'Miscellaneous'),
+  ("index", "mdds", "mdds Documentation",
+   "Kohei Yoshida", "mdds", "One line description of project.",
+   "Miscellaneous"),
 ]
 
 breathe_projects = {"mdds": "./_doxygen/xml"}
 breathe_default_project = "mdds"
-breathe_default_members = ('members', 'undoc-members')
+breathe_default_members = ("members", "undoc-members")
